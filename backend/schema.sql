9a50912be68a6d238ec2a6a6005c09f3ee53795f
nataliaaolmo <natolmvil@alum.us.es>
2025-07-30 18:28:30 +0100

    fix: backend

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS subscribers (
   id SERIAL PRIMARY KEY,
   email TEXT NOT NULL UNIQUE,
-  phone_number TEXT,
+  phone TEXT,
   name TEXT,
   interests TEXT[],
   subscribed_at TIMESTAMP DEFAULT NOW()
